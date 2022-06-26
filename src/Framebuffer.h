83900c2371a21abf387b8216fbf50cd76e42caf5
subje2 <sdbb2q@gmail.com>
2022-06-26 14:42:57 +0900

    Add files via upload

---
+++ src/Framebuffer.h
@@ -0,0 +1,12 @@
+//used poncho's code
+
+#pragma once
+#include <stddef.h>
+
+struct Framebuffer{
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} ;
---
+++ src/math.h
@@ -0,0 +1,9 @@
+
+
+#pragma once
+
+
+struct Point{
+    unsigned int X;
+    unsigned int Y;
+} ;
