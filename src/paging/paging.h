80f826282ecc7e14465dd81631923fa5397af3b6
subje2 <sdbb2q@gmail.com>
2022-06-27 18:31:45 +0900

    Update paging.h

--- src/paging/paging.h
+++ src/paging/paging.h
@@ -1,24 +1,28 @@
-#include "paging.h"
+#pragma once
+#include <stdint.h>
 
-void PageDirectoryEntry::SetFlag(PT_Flag flag, bool enabled){
-    uint64_t bitSelector = (uint64_t)1 << flag;
-    Value &= ~bitSelector;
-    if (enabled){
-        Value |= bitSelector;
-    }
-}
+enum PT_Flag {
+    Present = 0,
+    ReadWrite = 1,
+    UserSuper = 2,
+    WriteThrough = 3,
+    CacheDisabled = 4,
+    Accessed = 5,
+    LargerPages = 7,
+    Custom0 = 9,
+    Custom1 = 10,
+    Custom2 = 11,
+    NX = 63 // only if supported
+};
 
-bool PageDirectoryEntry::GetFlag(PT_Flag flag){
-    uint64_t bitSelector = (uint64_t)1 << flag;
-    return Value & bitSelector > 0 ? true : false;
-}
+struct PageDirectoryEntry {
+    uint64_t Value;
+    void SetFlag(PT_Flag flag, bool enabled);
+    bool GetFlag(PT_Flag flag);
+    void SetAddress(uint64_t address);
+    uint64_t GetAddress();
+};
 
-uint64_t PageDirectoryEntry::GetAddress(){
-    return (Value & 0x000ffffffffff000) >> 12;
-}
-
-void PageDirectoryEntry::SetAddress(uint64_t address){
-    address &= 0x000000ffffffffff;
-    Value &= 0xfff0000000000fff;
-    Value |= (address << 12);
-}
+struct PageTable { 
+    PageDirectoryEntry entries [512];
+}__attribute__((aligned(0x1000)));
